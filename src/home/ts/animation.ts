1faf2531247019199b87621bc04fb140a5ddbb7b
ivan1 <pupov435@gmail.com>
2023-04-02 13:28:09 +0700

    f

---
+++ src/home/ts/animation.ts
@@ -0,0 +1,32 @@
+import { makeSpanWith } from "./utils/dom.js";
+import { ITitle } from "./interfaces/ITitle.js";
+import { titleInfo,
+        subtitleInfo } from "./main.js";
+
+export function animateHeader(button: HTMLButtonElement, title: ITitle = titleInfo): void {
+    let curIndex = 0
+
+    let currentSpan: HTMLSpanElement
+    const animationInterval = setInterval(() => {
+        if (currentSpan != undefined) currentSpan.classList.remove(title.showModeName)
+
+        currentSpan = makeSpanWith(title.letterArray[curIndex], title.showModeName)
+        title.wrapper.appendChild(currentSpan)
+
+        if (curIndex == title.lineBreakIndex) title.wrapper.appendChild(document.createElement('br'))
+        if (curIndex == title.letterArray.length - 1)  {
+            stopAnimationInterval()
+            if (!title.isLast) animateHeader(button, subtitleInfo)
+            else {
+                currentSpan.classList.remove(title.showModeName)
+                button.style.opacity = "1"
+            }
+        }
+
+        curIndex += 1
+    }, title.displaySpeed)
+
+    function stopAnimationInterval(): void {
+        clearInterval(animationInterval)
+    }
+}
---
+++ src/home/ts/main.ts
@@ -0,0 +1,58 @@
+import { ITitle } from "./interfaces/ITitle.js";
+import { INavOptions } from "./interfaces/INavOptions.js";
+import { animateHeader } from "./animation.js";
+import {
+    toggleDisplayName,
+    navigationButtonHandle,
+    buttonStartHandle } from "./eventHandlers.js";
+
+const mainScreen = document.querySelector('.main-screen') as HTMLDivElement
+const buttonStart = document.querySelector('.button-start') as HTMLButtonElement
+
+const navigationButtons = Array.from<HTMLDivElement>(document.querySelectorAll('.nav-btn div'))
+const algorithmScreen = document.querySelector('.algorithms-screen') as HTMLDivElement
+
+const elTitle = document.querySelector('.title') as HTMLDivElement
+const elSubtitle = document.querySelector('.subtitle') as HTMLDivElement
+
+const lettersOfTitle = Array.from("Привет! Я Алготрон")
+const lettersOfSubtitle = Array.from("Твой гид в мире алгоритмов.")
+
+const showModeName = 'anim-letter'
+
+export const titleInfo: ITitle = {
+    wrapper: elTitle,
+    letterArray: lettersOfTitle,
+    lineBreakIndex: 6,
+    showModeName: showModeName,
+    displaySpeed: 80,
+    isLast: false
+}
+
+export const subtitleInfo: ITitle = {
+    wrapper: elSubtitle,
+    letterArray: lettersOfSubtitle,
+    displaySpeed: 15,
+    isLast: true
+}
+
+const navOptions: INavOptions = {
+    buttons: navigationButtons,
+    screen: algorithmScreen,
+    currentScreenIndex: 0
+}
+
+animateHeader(buttonStart)
+
+buttonStart.addEventListener('click', () => {
+    buttonStartHandle(mainScreen)
+})
+
+navigationButtons.forEach(element => { element.addEventListener('mouseover', toggleDisplayName) })
+navigationButtons.forEach(element => { element.addEventListener('mouseleave', toggleDisplayName) })
+
+navigationButtons.forEach(el => {
+    el.addEventListener('click', (event: MouseEvent) => {
+        navigationButtonHandle(event, navOptions)
+    })
+})
