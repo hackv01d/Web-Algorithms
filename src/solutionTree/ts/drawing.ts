107de17314ece6826d861916f231acc13eca7da2
ivan1 <pupov435@gmail.com>
2023-04-07 18:29:39 +0700

    added ability enter text create tree and find it

--- src/solutionTree/ts/drawing.ts
+++ src/solutionTree/ts/drawing.ts
@@ -1,42 +1,27 @@
-// import { parseCsv, readCsvFile } from "./parseData";
-// import { Tree} from "./tree";
+
+import {Tree} from "./tree.js";
 
 
-// async function decisionTree(): Promise<void> {
-//   const csv =  'src/solutionTree/data/weather.csv';
-//    const data: string[][] = readCsvFile(csv);
-  
-  
-//   await new Promise(resolve => setTimeout(resolve, 2000));
-//   console.log(data);
-//   const tree = new Tree(data);
-//   const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-//   console.log(tree.rootNode);
-  
-  
-  
-//   readline.question('Введите ваш запрос: ', (name: string) => {
-//     const inputObject = name.split(',');
-//     console.log(inputObject);
-    
-//     console.log(tree.traverseTree(tree.rootNode, inputObject).attribute);
-//     readline.close();
-//   });
-    
 
-// }
-
-// decisionTree();
-
-const myInput = document.getElementById('myInput') as HTMLInputElement;
-const myButton = document.getElementById('myButton');
-
-myButton.addEventListener('click', () => {
-  const inputText = myInput.value;
-  console.log(inputText);
-});
 // добавить функцию визуализации созданного дерева
 // добавить функцию визуализации поиска пути и вывод результата targetValue
+const dataInput = document.getElementById("data-input") as HTMLInputElement;
+const submitButton = document.getElementById("submit-button");
+
+const dataInputTwo = document.getElementById("data-input_two") as HTMLInputElement;
+const submitButtonTwo = document.getElementById("submit-button_two");
+let tree: Tree;
+submitButton.addEventListener("click", () => {
+  const data: string = dataInput.value;
+  const rows: string[] = data.split('\n');
+  const matrix: string[][] = rows.map((row) => row.split(';'));
+  tree = new Tree(matrix);
+  console.log(tree.rootNode);
+});
+
+
+submitButtonTwo.addEventListener("click", () => {
+  const data: string = dataInputTwo.value;
+  const rows: string[] = data.split(';');
+  console.log(tree.traverseTree(tree.rootNode, rows).attribute);
+});
--- src/solutionTree/ts/parseData.ts
+++ src/solutionTree/ts/parseData.ts
@@ -1,24 +1,8 @@
 // import fs from 'fs';
-// import csvParser from 'csv-parser';
+
 // import * as readline from 'readline';
 
-// let separatorSymbol = ","; // добавить выбор между , и ;
 
-
-
-// export function parseCsv(way: string) : string[][]{
-//     const results: string[][] = [];
-
-//     fs.createReadStream(way)
-//         .pipe(csvParser({ separator: separatorSymbol}))
-//         .on('data', (row: string[]) => {
-//             results.push(row);
-//           })
-//         .on('end', () => { return results;
-//         });
-//         return results;
-    
-//     }
 // export function readCsvFile(way: string): Promise<string[]> {
 //     return new Promise<string[]>((resolve, reject) => {
 //         const fileStream = fs.createReadStream(way);
@@ -41,19 +25,3 @@
 //         });
 //     });
 //     }
-// // export async function parseCsv(way: string, separatorSymbol: string) : Promise<string[][]>{
-// //     const results: string[][] = [];
-
-// //     return new Promise((resolve, reject) => {
-// //         fs.createReadStream(way)
-// //             .pipe(csvParser({ separator: separatorSymbol}))
-// //             .on('data', (it) => results.push(it))
-// //             .on('end', () => {
-// //                 console.log(results);
-// //                 resolve(results);
-// //             })
-// //             .on('error', (err) => {
-// //                 reject(err);
-// //             });
-// //     });
-// // }
